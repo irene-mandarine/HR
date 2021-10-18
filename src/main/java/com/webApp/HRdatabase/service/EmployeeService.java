2569d0880835363b973a21f68feaf7a16cf35e8c
ifiodorova <myefij@gmail.com>
2021-10-18 16:29:18 +0300

    Web app using JDBC

--- src/main/java/com/webApp/HRdatabase/service/EmployeeService.java
+++ src/main/java/com/webApp/HRdatabase/service/EmployeeService.java
@@ -1,17 +1,48 @@
 package com.webApp.HRdatabase.service;
 
+import com.webApp.HRdatabase.data.Employee;
 import com.webApp.HRdatabase.repository.EmployeeRepository;
+import com.webApp.HRdatabase.validation.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final ValidationService validationService;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository,
+                           ValidationService validationService) {
         this.employeeRepository = employeeRepository;
+        this.validationService = validationService;
     }
 
+    public List<Employee> getEmployees() {
+        return employeeRepository.getEmployees();
+    }
+
+    public Employee getEmployee(Long id) {
+        validationService.validateId(id);
+        return employeeRepository.getEmployee(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("No such employee."));
+    }
+
+    public Employee addEmployee(Employee employee) {
+        validationService.validateEmployee(employee);
+        if (employeeRepository.findByEmail(employee.getEmail())) {
+            throw new IllegalArgumentException("Employee with such email already exists.");
+        }
+        return employeeRepository.addEmployee(employee);
+    }
+
+    public Employee editEmployee(Employee employee, Long id) {
+        getEmployee(id);
+        validationService.validateEmployee(employee);
+        return employeeRepository.editEmployee(employee, id);
+    }
 }
